0480580696194595589523d4650786762054da78
jpb <jpb.06@outlook.com>
2018-05-01 22:39:32 +0200

    Base dev env import

---
+++ build/webpack.config.dev.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const webpack = require('webpack');
+const {
+    VueLoaderPlugin
+} = require('vue-loader');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devServer: {
+        hot: true
+    },
+    module: {
+        rules: [{
+                test: /\.vue$/,
+                use: 'vue-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            { // should come after vue loader
+                test: /\.js$/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin(),
+        new webpack.HotModuleReplacementPlugin(),
+        new htmlWebpackPlugin({
+            filename: 'index.html',
+            template: 'index.html',
+            inject: true
+        })
+    ]
+};
