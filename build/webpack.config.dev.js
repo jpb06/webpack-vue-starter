d472a98efc8d341166fba0cf64ff5a61aa12d7f2
jpb <jpb.06@outlook.com>
2018-05-02 19:54:51 +0200

    Setting up linting

--- build/webpack.config.dev.js
+++ build/webpack.config.dev.js
@@ -1,38 +1,43 @@
 'use strict'
 
-const webpack = require('webpack');
+const webpack = require('webpack')
 const {
-    VueLoaderPlugin
-} = require('vue-loader');
-const htmlWebpackPlugin = require('html-webpack-plugin');
+  VueLoaderPlugin
+} = require('vue-loader')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'development',
-    devServer: {
-        hot: true
+  mode: 'development',
+  devServer: {
+    hot: true
+  },
+  module: {
+    rules: [{
+      test: /\.(js|vue)$/,
+      use: 'eslint-loader',
+      enforce: 'pre'
     },
-    module: {
-        rules: [{
-                test: /\.vue$/,
-                use: 'vue-loader'
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            },
-            { // should come after vue loader
-                test: /\.js$/,
-                use: 'babel-loader'
-            }
-        ]
+    {
+      test: /\.vue$/,
+      use: 'vue-loader'
     },
-    plugins: [
-        new VueLoaderPlugin(), // necessary for vue loader to work
-        new webpack.HotModuleReplacementPlugin(),
-        new htmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'index.html',
-            inject: true
-        })
+    {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    },
+    { // should come after vue loader
+      test: /\.js$/,
+      use: 'babel-loader'
+    }
     ]
-};
+  },
+  plugins: [
+    new VueLoaderPlugin(), // necessary for vue loader to work
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'index.html',
+      inject: true
+    })
+  ]
+}
