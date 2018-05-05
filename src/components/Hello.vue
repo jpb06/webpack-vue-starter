86e042eb51ef20ff36b016de29ab15bbfa498c79
jpb <jpb.06@outlook.com>
2018-05-05 23:49:14 +0200

    Adding components

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -4,6 +4,7 @@
 
 <script>
 export default {
+  name: 'Hello',
   props: {
     name: {
       type: String,
---
+++ src/components/Message.vue
@@ -0,0 +1,15 @@
+<template>
+  <h1> {{ msg }}</h1>
+</template>
+
+<script>
+export default {
+  name: 'Message',
+  props: {
+    msg: {
+      type: String,
+      default: 'default message'
+    }
+  }
+}
+</script>
