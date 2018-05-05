8bc4946921e51fa34739a2f39d3cc239e9c65d54
jpb <jpb.06@outlook.com>
2018-05-05 23:48:56 +0200

    Getting linting configured

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,11 +2,22 @@ module.exports = {
     parserOptions: {
         parser: 'babel-eslint'
     },
+    env: {
+        browser: true,
+        node: true,
+        mocha: true
+    },
+    globals: {
+        expect: true
+    },
     extends: [
         'plugin:vue/recommended',
         'standard'
     ],
     plugins: [
         'vue'
-    ]
+    ],
+    rules: {
+        "vue/max-attributes-per-line": "off"
+    }
 }
