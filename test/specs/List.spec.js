734339c297067661289a4befb5f5aac10616dbcc
jpb <jpb.06@outlook.com>
2018-05-05 23:49:58 +0200

    Tests

---
+++ test/specs/List.spec.js
@@ -0,0 +1,20 @@
+import { shallow } from '@vue/test-utils'
+import List from '@/components/List.vue'
+
+describe('List.vue', () => {
+  it('renders li for each item in props.items', () => {
+    const items = ['a', 'b']
+    const wrapper = shallow(List, {
+      propsData: { items }
+    })
+    expect(wrapper.findAll('li')).toHaveLength(items.length)
+  })
+
+  it('matches snapshot', () => {
+    const items = ['item 1', 'item 2']
+    const wrapper = shallow(List, {
+      propsData: { items }
+    })
+    expect(wrapper.html()).toMatchSnapshot()
+  })
+})
---
+++ test/specs/MessageToggle.spec.js
@@ -0,0 +1,14 @@
+import { shallow } from '@vue/test-utils'
+import MessageToggle from '@/components/MessageToggle.vue'
+import Message from '@/components/Message.vue'
+
+describe('MessageToggle.vue', () => {
+  it('toggles msg passed to Message when button is clicked', () => {
+    const wrapper = shallow(MessageToggle)
+    const button = wrapper.find('#toggle-message')
+    const MessageComponent = wrapper.find(Message)
+    expect(MessageComponent.props()).toEqual({msg: 'message'})
+    button.trigger('click')
+    expect(MessageComponent.props()).toEqual({msg: 'toggled message'})
+  })
+})
